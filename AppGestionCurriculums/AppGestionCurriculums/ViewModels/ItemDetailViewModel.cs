64dc276844f705790bf67824b627b50669763fa5
Brian Alejandro Casas López <bralcasaslo@ittepic.edu.mx>
2018-11-20 19:20:07 -0700

    Primer commit - MasterDetail

---
+++ AppGestionCurriculums/AppGestionCurriculums/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppGestionCurriculums.Models;
+
+namespace AppGestionCurriculums.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
