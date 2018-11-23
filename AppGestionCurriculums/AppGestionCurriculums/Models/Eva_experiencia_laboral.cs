a9c227c908845e825e1e0feeebaff89a725aaf14
alegreman <alegreman09@gmail.com>
2018-11-23 02:02:26 -0700

    feat: agregue tabla eva_experiencia_laboral al model

---
+++ AppGestionCurriculums/AppGestionCurriculums/Models/Eva_experiencia_laboral.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppGestionCurriculums.Models
+{
+    class Eva_experiencia_laboral
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int16 IdCurriculo { get; set; }
+        public Int16 IdExperiencia { get; set; }
+        public string DesOrganizacion { get; set; }
+        public string Detalle { get; set; }
+        public string Siglas { get; set; }
+        public DateTime FechaIni { get; set; }
+        public DateTime FechaFin { get; set; }
+        public Int16 IdTIpoGenGiro { get; set; }
+        public Int16 IdGenGiro { get; set; }
+        public DateTime FechaReg { get; set; }
+        public DateTime FechaUltMod { get; set; }
+        public string UsuarioReg { get; set; }
+        public string UsurioMod { get; set; }
+        public string Activo { get; set; }
+        public string Borrado { get; set; }
+    }
+}
