eaf7e1cf58c973b823011a7519f3315228ee6f69
jjesusmonroy <jodemonroysa@ittepic.edu.mx>
2018-12-01 13:18:20 -0600

    added some changes from rama-briand and adapted them to mine

---
+++ AppGestionCurriculums/AppGestionCurriculums.UWP/SQLite/FicConfigSQLiteUWP.cs
@@ -0,0 +1,24 @@
+﻿using AppGestionCurriculums.Interfaces.SQLite;
+using AppGestionCurriculums.UWP.SQLite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FicConfigSQLiteUWP))]
+namespace AppGestionCurriculums.UWP.SQLite
+{
+    public class FicConfigSQLiteUWP : IFicConfigSQLite
+    {
+        public string FicGetDataBasePath()
+        {
+            Debug.WriteLine(Path.Combine(ApplicationData.Current.LocalFolder.Path, AppSettings.FicDataBaseName));
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, AppSettings.FicDataBaseName);
+        }
+    }
+}
